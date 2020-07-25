ea746e37d3858035480171df66c8b68592dd46da
jerry <jerrylai19990120@gmail.com>
2020-07-24 22:30:16 -0400

    server update

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/pub'));
 
+app.get('/', (req, res)=>{
+    res.sendFile("./examples.html");
+    res.sendFile("./examples.js");
+    res.sendFile("./examples.css");
+
+});
+
 app.listen(port, ()=>{
     console.log(`listen on port ${port}`);
 });
