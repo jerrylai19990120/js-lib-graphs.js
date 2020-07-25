a9ba9ce5c49b045018126498415d0bfdd1476d7b
jerry <jerrylai19990120@gmail.com>
2020-07-25 13:41:19 -0400

    examples page

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(express.static(__dirname + '/pub'));
 
 app.get('/', (req, res)=>{
-    res.send("<h1>successful connected!</h1>");
+    res.sendFile('./examples.html');
 });
 
 app.listen(port, ()=>{
