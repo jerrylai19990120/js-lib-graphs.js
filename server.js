a728bbba6295e0215217ff56c609621d54ad0d5d
jerry <jerrylai19990120@gmail.com>
2020-07-24 22:34:55 -0400

    server update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(express.static(__dirname + '/pub'));
 
 app.get('/', (req, res)=>{
-    res.sendFile("/");
+    res.sendFile("/examples.html");
 });
 
 app.listen(port, ()=>{
