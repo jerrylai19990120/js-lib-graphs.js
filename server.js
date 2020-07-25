b2f45f07eac85dd778c27034b4a2b296548bb014
jerry <jerrylai19990120@gmail.com>
2020-07-25 02:58:05 -0400

    server update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(express.static(__dirname + '/pub'));
 
 app.get('/', (req, res)=>{
-    res.send("<h1>successful</h1>");
+    res.send("<h1>successful connected!</h1>");
 });
 
 app.listen(port, ()=>{
