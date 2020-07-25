47a6b6a3041423a867de29b921229c2e5eca8b6d
jerry <jerrylai19990120@gmail.com>
2020-07-24 22:23:06 -0400

    server update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname, '/pub'));
+app.use(express.static(__dirname + '/pub'));
 
 app.listen(port, ()=>{
     console.log(`listen on port ${port}`);
