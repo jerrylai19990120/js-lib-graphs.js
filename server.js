bbd61c2078e974f0528c29e4a944a7605c396105
jerry <jerrylai19990120@gmail.com>
2020-07-26 14:25:15 -0400

    update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -7,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(express.static(__dirname + '/pub'));
 
 app.get('/', (req, res)=>{
-    res.send("<h1>Successfully connected to server.</h1>");
+    res.sendFile(path.join(__dirname, '/pub/examples.html'));
 });
 
 app.listen(port, ()=>{
