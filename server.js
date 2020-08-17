6e4362be282d3dbfb0a1e18eb90c166053b35917
jerry <jerrylai19990120@gmail.com>
2020-08-17 12:20:56 -0400

    files added

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '/pub/examples.html'));
 });
 
+app.get('/GetStarted', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/pub/GetStarted.html'));
+})
+
+app.get('/Documentation', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/pub/Documentation.html'));
+})
+
 app.listen(port, ()=>{
     console.log(`listen on port ${port}`);
 });
